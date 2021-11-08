b15dcbe62522f945111042874d8cd275e0d616b1
dnflekals <dnflekals@gmail.com>
2021-11-08 20:18:03 +0900

    refactor: Random 값 생성 이후 각 자리 숫자 맞출때까지 game이 진행되도록 refactoring

---
+++ src/main/java/baseball/SettingRandomNumber.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import java.util.HashSet;
+import java.util.Set;
+import utils.RandomUtils;
+
+public class SettingRandomNumber {
+
+    private Set<Integer> set = new HashSet<>();
+    protected void setRandomValue(int[] answerNumber) {
+        set.clear();
+        int countRandomValue = 0;
+
+        int baseballNumberLength = 3;
+        while (countRandomValue < baseballNumberLength) {
+            int num = RandomUtils.nextInt(1, 9);
+
+            if (!set.contains(num)) {
+                set.add(num);
+                answerNumber[countRandomValue] = num;
+                countRandomValue++;
+            }
+        }
+    }
+}
